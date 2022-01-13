ffab513d6f6034ca4cc76d6f533bb6239d9b927c
OLIPLICHE <olipliche@gmail.com>
2022-01-13 05:49:14 +0100

    Setting application Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :views
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "splash#index"
 
-  resources :splash, only: %i[index]
+  get "/splash", to: "splash#index"
+  # resources :splash, only: %i[index]
 end
