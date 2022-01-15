ba81f8f35a37279f276250f59db0c9070c39b1e6
OLIPLICHE <olipliche@gmail.com>
2022-01-15 14:20:32 +0100

    solve buggs

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,2 +1,3 @@
 class EntitiesController < ApplicationController
+    
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
       user ||= User.new # guest user (not logged in)
-      if user.role == admin?
+      if user.role == 'admin'
         can :manage, :all
       else
         can :manage, Group, user_id: user.id
