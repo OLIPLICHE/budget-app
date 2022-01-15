e064e9e5a3b06396ba99eb50ab139fee344838a1
OLIPLICHE <olipliche@gmail.com>
2022-01-15 12:00:03 +0100

    Add actions New and Create controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,4 +18,26 @@ class GroupsController < ApplicationController
     @total = @group.entities.sum(:amount)
   end
 
+  def new
+    @user = current_user
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new group_params
+    @group.user = current_user
+
+    if @group.save
+      redirect_to user_groups_path(current_user), notice: 'Category added successfully'
+    else
+      render :new
+    end
+  end
+  
+  private 
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
+
 end
