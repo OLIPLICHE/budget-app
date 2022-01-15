d60087a89764cc16ec2795718ea8c92a7fd95159
OLIPLICHE <olipliche@gmail.com>
2022-01-15 04:25:34 +0100

    Set index action in group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,14 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource
+
+  def index
+    @user = current_user
+    @groups = Group.order(:created_at).where(user: @user)
+    @total = 0
+    @groups.each do |group|
+      @total += group.entities.sum(:amount)
+    end
+  end
+
 end
